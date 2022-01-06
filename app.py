17e27cc7904fc167e5ea31d929ce4d6d451682c8
Ephron Martin <ephronmartin2016@gmail.com>
2022-01-06 17:31:04 +0530

    AQI of bangalore

---
+++ app.py
@@ -0,0 +1,27 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template, url_for
+import pickle
+
+
+
+app = Flask(__name__)
+model = pickle.load(open('Finalmodel.pkl','rb'))
+
+@app.route('/')
+def home():
+    #return 'Hello World'
+    return render_template('home.html')
+    #return render_template('index.html')
+
+@app.route('/predict',methods = ['POST'])
+def predict():
+    int_features = [float(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+    print(prediction[0])
+
+    #output = round(prediction[0], 2)
+    return render_template('home.html', prediction_text="AQI for Bangalore {}".format(np.round(prediction[0],2)))
+
+if __name__ == '__main__':
+    app.run(debug=True) 

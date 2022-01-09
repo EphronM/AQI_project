14a8a258ab832fd5728a4f418d58d9a14bd968f5
Ephron Martin <94764266+EphronM@users.noreply.github.com>
2022-01-09 17:50:49 +0530

    Add files via upload

---
+++ AutoML/tpot_aqi_pipeline.py
@@ -0,0 +1,26 @@
+import numpy as np
+import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.feature_selection import VarianceThreshold
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.pipeline import make_pipeline, make_union
+from sklearn.preprocessing import PolynomialFeatures
+from tpot.builtins import StackingEstimator
+
+# NOTE: Make sure that the outcome column is labeled 'target' in the data file
+tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
+features = tpot_data.drop('target', axis=1)
+training_features, testing_features, training_target, testing_target = \
+            train_test_split(features, tpot_data['target'], random_state=None)
+
+# Average CV score on the training set was: -1564.914478643211
+exported_pipeline = make_pipeline(
+    StackingEstimator(estimator=RandomForestRegressor(bootstrap=True, max_features=0.7000000000000001, min_samples_leaf=1, min_samples_split=9, n_estimators=100)),
+    PolynomialFeatures(degree=2, include_bias=False, interaction_only=False),
+    VarianceThreshold(threshold=0.1),
+    KNeighborsRegressor(n_neighbors=77, p=1, weights="distance")
+)
+
+exported_pipeline.fit(training_features, training_target)
+results = exported_pipeline.predict(testing_features)
